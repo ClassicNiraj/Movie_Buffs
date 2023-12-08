64b8c74e1ab66e92662b8c599dfdf7452d7996a7
Niraj Bhakta <nirajbhakta111@gmail.com>
2023-12-07 22:48:20 -0600

    test push 5

--- app/src/main/java/com/example/moviebuffs/ui/theme/MovieDetailScreen.kt
+++ app/src/main/java/com/example/moviebuffs/ui/theme/MovieDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.example.moviebuffs.ui.theme
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -11,24 +10,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.moviebuffs.network.Movie
+import com.example.moviebuffs.R
 
 @Composable
-fun MovieDetailScreen(
-    movie: Movie
-) {
-    fun calculateWindowSizeClass(configuration: Configuration) {
-
-    }
+fun MovieDetailScreen(movie: Movie) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
-            .padding(top = 56.dp) // Padding for TopAppBar
+            .padding(top = 56.dp)
     ) {
         Image(
             painter = painterResource(id = R.drawable.loading_img),
@@ -38,30 +32,37 @@ fun MovieDetailScreen(
                 .height(350.dp),
             contentScale = ContentScale.Crop
         )
-        Column(modifier = Modifier.padding(8.dp)) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = movie.title,
                 style = MaterialTheme.typography.headlineLarge,
                 fontWeight = FontWeight.Bold
             )
-            @Composable
-            fun MoviesListAndDetails(viewModel: MovieViewModel) {
-                val configuration = LocalConfiguration.current
-                val windowSizeClass = calculateWindowSizeClass(configuration)
-
-                if (windowSizeClass.widthSizeClass == WindowSizeClass.WidthSizeClass.EXPANDED) {
-                    Row(modifier = Modifier.fillMaxSize()) {
-                        // Assuming MovieList and MovieDetailScreen are already defined
-                        MovieList(viewModel = viewModel, contentPadding = PaddingValues(0.dp))
-                        viewModel.Movie?.let { movie ->
-                            MovieDetailScreen(movie = movie)
-                        }
-                    }
-                } else {
-                    MovieList(viewModel = viewModel, contentPadding = PaddingValues(0.dp))
-                }
-            }
+            Text(
+                text = "Release Date: ${movie.release_date}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Rating: ${movie.content_rating}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Review Score: ${movie.review_score}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Length: ${movie.length}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = movie.description,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
